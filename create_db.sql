f198a254267b871793913a7b473a171a72bbe5ed
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2022-08-21 20:59:21 +0200

    Guardians data processing

---
+++ create_db.sql
@@ -0,0 +1,15 @@
+CREATE TABLE guardians (
+    id TEXT NOT NULL PRIMARY KEY,
+    address_or_ens TEXT NOT NULL,
+    address TEXT,
+    chain_id INTEGER NOT NULL DEFAULT 1,
+    name TEXT NOT NULL,
+    ens TEXT,
+    image_url TEXT,
+    image_src BLOB,
+    image_1x BLOB,
+    image_2x BLOB,
+    image_3x BLOB,
+    reason TEXT,
+    contribution TEXT
+)
