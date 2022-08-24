41fa2c5e5248000de8f60673db9e9609e63d61e2
Vitaly Katz <vitaly@gnosis.io>
2022-08-24 13:27:24 +0200

    Vesting data and hash calculation

---
+++ vestings/vesting.py
@@ -0,0 +1,61 @@
+from hexbytes import HexBytes
+from eth_abi import abi
+from eth_abi.packed import encode_packed
+from web3 import Web3
+
+
+VESTING_TYPEHASH = HexBytes("0x43838b5ce9ca440d1ac21b07179a1fdd88aa2175e5ea103f6e37aa6d18ce78ad")
+
+DOMAIN_SEPARATOR_TYPEHASH = HexBytes("0x47e79534a245952e8b16893a336b85a3d9ea9fa8c573f3d803afb92a79469218")
+
+EMPTY_HASH = Web3.solidityKeccak(['bytes'], [bytes(HexBytes("0x"))])
+
+
+class Vesting:
+
+    def __init__(self, id: str, type, account, curveType, durationWeeks, startDate, amount, proof: list):
+        self.id = id
+        self.type = type
+        self.account = account
+        self.curveType = curveType
+        self.durationWeeks = durationWeeks
+        self.startDate = startDate
+        self.amount = amount
+        self.proof = proof
+
+    def calculateHash(self, airdrop_address, chain_id):
+        domain_separator = Web3.solidityKeccak(
+            ['bytes'],
+            [
+                abi.encode(
+                    ('bytes32', 'uint256', 'address'),
+                    (bytes(DOMAIN_SEPARATOR_TYPEHASH), chain_id, airdrop_address.hex())
+                )
+            ]
+        )
+
+        vesting_data_hash = Web3.solidityKeccak(
+            ['bytes'],
+            [
+                abi.encode(
+                    ('bytes32', 'address', 'uint8', 'bool', 'uint16', 'uint64', 'uint128'),
+                    (bytes(VESTING_TYPEHASH), self.account, self.curveType, False,
+                     int(self.durationWeeks), int(self.startDate.timestamp()), int(self.amount))
+                )
+            ]
+        )
+
+        vesting_id = Web3.solidityKeccak(
+            ['bytes'],
+            [
+                encode_packed(
+                    ('bytes1', 'bytes1', 'bytes', 'bytes'),
+                    (HexBytes(0x19), HexBytes(0x01), domain_separator, vesting_data_hash)
+                )
+            ]
+        )
+
+        return HexBytes(vesting_id).hex()
+
+
+
