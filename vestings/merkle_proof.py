1ab93b3b27b506a2a4389b590e7d65c7d03ea22f
Vitaly Katz <vitaly@gnosis.io>
2022-09-19 12:07:35 +0200

    Extract proof from pre-generated vestings tree

--- vestings/merkle_proof.py
+++ vestings/merkle_proof.py
@@ -20,8 +20,50 @@ def combine_and_hash(leaf1: str, leaf2: str):
     return HexBytes(combined_hash).hex()
 
 
-def generate(input, element=None):
+def generate_vestings_tree(elements):
+    count = len(elements)
+    previous_level = elements.copy()
+    levels: list(list(str)) = [previous_level]
+    while count > 1:
+        current_level = []
+        i = 0
+        while i < count:
+            leaf1 = previous_level[i]
+            leaf2 = EMPTY_HASH if i + 1 >= count else previous_level[i + 1]
+            if int.from_bytes(bytes(HexBytes(leaf1)), byteorder='big', signed=False) < int.from_bytes(bytes(HexBytes(leaf2)), byteorder='big', signed=False):
+                current_level.append(combine_and_hash(leaf1, leaf2))
+            else:
+                current_level.append(combine_and_hash(leaf2, leaf1))
+            i = i + 2
+        count = math.ceil(count / 2)
+        previous_level = current_level.copy()
+        levels.append(previous_level)
 
+    return levels
+
+
+def extract_proof(vestings_tree, element):
+    proof = []
+    current_level_index = 0
+    element_index = vestings_tree[current_level_index].index(element)
+
+    level_count = len(vestings_tree)
+    while current_level_index < level_count - 1:
+        current_level = vestings_tree[current_level_index]
+        if element_index % 2 == 0:
+            if element_index + 1 >= len(current_level):
+                proof.append(EMPTY_HASH)
+            else:
+                proof.append(current_level[element_index + 1])
+        else:
+            proof.append(current_level[element_index - 1])
+        element_index = math.floor(element_index / 2)
+        current_level_index = current_level_index + 1
+
+    return proof
+
+
+def generate(input, element=None):
     proof = []
     elements = input.copy()
     count = len(elements)
--- vestings/proof_generator.py
+++ vestings/proof_generator.py
@@ -5,11 +5,11 @@ from joblib import Parallel, delayed
 from database import get_db
 
 
-def generate_and_safe_proof_for_vesting(db_file, vesting_ids, vesting_id):
+def generate_and_safe_proof_for_vesting(db_file, vestings_tree, vesting_id):
 
     db = next(get_db(db_file))
 
-    proof, root = merkle_proof.generate(vesting_ids, vesting_id)
+    proof = merkle_proof.extract_proof(vestings_tree, vesting_id)
     proof_index = 0
     for part in proof:
         proof_model = ProofModel(
@@ -34,8 +34,9 @@ def generate_and_save_proofs(db_file, type, chain_id):
     db = next(get_db(db_file))
     vestings = db.query(VestingModel).filter(VestingModel.type == type and VestingModel.chain_id == chain_id)
     vesting_ids = list(map(lambda vesting: vesting.vesting_id, vestings))
+    vestings_tree = merkle_proof.generate_vestings_tree(vesting_ids)
 
-    Parallel(n_jobs=-1)((delayed(generate_and_safe_proof_for_vesting)(db_file, vesting_ids, vesting_id) for vesting_id in vesting_ids))
+    Parallel(n_jobs=-1)((delayed(generate_and_safe_proof_for_vesting)(db_file, vestings_tree, vesting_id) for vesting_id in vesting_ids))
 
 
 def generate_and_print_root(db: orm.Session, type, chain_id):
