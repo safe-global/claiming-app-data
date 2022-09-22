689ac5f0fc9fe8bfbad41590a0d9baec34073acf
Vitaly Katz <vitaly@gnosis.io>
2022-09-22 17:26:49 +0200

    Conversion and scaling of guardian images (#8)
    * Fix svg to png conversion
    * Adjust deployment pipeline to process guardian data
    * Add infura id to env

--- guardians/import_guardians.py
+++ guardians/import_guardians.py
@@ -36,7 +36,25 @@ from reportlab.graphics import renderPM
 from PIL import Image
 from PIL.Image import Resampling
 
+
+from wand.api import library
+import wand.color
+import wand.image
+
 import json
+
+DB_FILE = 'intermediates/guardians.sqlite'
+CSV_FILE = 'assets/guardians.csv'
+IMAGE_DIR = 'assets/images'
+
+
+def clean_guardians():
+    con = sqlite3.connect(DB_FILE)
+    cur = con.cursor()
+
+    cur.execute('''DELETE FROM guardians''')
+    con.commit()
+    con.close()
 
 
 # guardians.csv columns
@@ -51,11 +69,11 @@ import json
 # 8 - network id
 # 9 - tags
 def import_guardians():
-    con = sqlite3.connect('intermediates/guardians.sqlite')
+    con = sqlite3.connect(DB_FILE)
     cur = con.cursor()
 
     # import data from csv
-    with open('assets/guardians.csv', newline='') as csvfile:
+    with open(CSV_FILE, newline='') as csvfile:
         guardian_reader = csv.reader(csvfile)
         # skip header row
         next(guardian_reader)
@@ -79,11 +97,12 @@ def resolve_ens(ens):
     address = ns.address(ens)
     return address
 
+
 def resolve_ens_names():
     address_re = re.compile('(0x[a-fA-F0-9]{40})')
     ens_re = re.compile(r"(\S+\.\S+)")
 
-    con = sqlite3.connect('intermediates/guardians.sqlite')
+    con = sqlite3.connect(DB_FILE)
     cur_select = con.cursor()
     cur_update = con.cursor()
     for row in cur_select.execute("SELECT id, address_or_ens FROM guardians ORDER BY id"):
@@ -108,7 +127,7 @@ def resolve_ens_names():
 
 
 def download_images():
-    con = sqlite3.connect('intermediates/guardians.sqlite')
+    con = sqlite3.connect(DB_FILE)
     cur_select = con.cursor()
     cur_update = con.cursor()
     for row in cur_select.execute("SELECT id, image_url FROM guardians ORDER BY id"):
@@ -123,12 +142,13 @@ def download_images():
         con.commit()
     con.close()
 
+
 def substitute_images():
-    con = sqlite3.connect('intermediates/guardians.sqlite')
+    con = sqlite3.connect(DB_FILE)
     cur_select = con.cursor()
     cur_update = con.cursor()
 
-    directory = os.fsencode('assets/images')
+    directory = os.fsencode(IMAGE_DIR)
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         guardian_name = os.path.splitext(filename)[0]
@@ -144,16 +164,23 @@ def substitute_images():
 
 
 def svg_to_pil(svgbytes):
+
     buf = io.BytesIO(svgbytes)
-    rlg = svg2rlg(buf)
-    pil = renderPM.drawToPIL(rlg)
-    return pil
+
+    with wand.image.Image(blob=buf, format="svg", background=wand.color.Color('transparent')) as image:
+
+        with image.convert('png') as converted:
+            pil = Image.open(io.BytesIO(converted.make_blob()))
+            return pil
+
 
 def png_to_pil(pngbytes):
     return Image.open(io.BytesIO(pngbytes), formats=["PNG"])
 
+
 def jpeg_to_pil(jpegbytes):
     return Image.open(io.BytesIO(jpegbytes), formats=["JPEG"])
+
 
 def pil_to_png(pil, size):
     out = io.BytesIO()
@@ -161,8 +188,9 @@ def pil_to_png(pil, size):
     pil.save(out, format="PNG")
     return out.getvalue()
 
+
 def convert_images():
-    con = sqlite3.connect('intermediates/guardians.sqlite')
+    con = sqlite3.connect(DB_FILE)
     cur_select = con.cursor()
     cur_update = con.cursor()
 
@@ -196,8 +224,9 @@ def convert_images():
 
     con.close()
 
+
 def export_guardians_json():
-    con = sqlite3.connect('intermediates/guardians.sqlite')
+    con = sqlite3.connect(DB_FILE)
     cur_select = con.cursor()
     guardians = []
     for row in cur_select.execute("SELECT name, address, ens, reason, contribution FROM guardians ORDER BY name"):
@@ -216,21 +245,26 @@ def export_guardians_json():
     with open('../data/guardians/guardians.json', 'w', encoding='utf-8') as out_file:
         json.dump(guardians, out_file, ensure_ascii=False, indent=4)
 
+
 def export_guardian_images():
-    con = sqlite3.connect('intermediates/guardians.sqlite')
+    con = sqlite3.connect(DB_FILE)
     cur_select = con.cursor()
     for row in cur_select.execute("SELECT address, image_1x, image_2x, image_3x FROM guardians WHERE image_3x IS NOT NULL ORDER BY address"):
         address, image1, image2, image3 = row
         print(address)
-        with open(f"../data/guardians/images/{address}_1x.png", 'wb') as f1, open(
-                f"../data/guardians/images/{address}_2x.png", 'wb') as f2, open(
-            f"../data/guardians/images/{address}_3x.png", 'wb') as f3:
+        with open(f"../data/guardians/images/{address}_1x.png", 'wb') as f1, open(f"../data/guardians/images/{address}_2x.png", 'wb') as f2, open(f"../data/guardians/images/{address}_3x.png", 'wb') as f3:
             f1.write(image1)
             f2.write(image2)
             f3.write(image3)
     con.close()
 
+
 if __name__ == '__main__':
+
+    if not os.path.exists(os.path.dirname("../data/guardians/images/")):
+        os.makedirs("../data/guardians/images/")
+
+    clean_guardians()
     import_guardians()
     resolve_ens_names()
     download_images()
