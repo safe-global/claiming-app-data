6ded1d47604b415ff33557f46b11077537453b91
Vitaly Katz <vitaly@gnosis.io>
2022-09-22 14:35:21 +0200

    Fix svg conversion

--- guardians/import_guardians.py
+++ guardians/import_guardians.py
@@ -36,14 +36,17 @@ from reportlab.graphics import renderPM
 from PIL import Image
 from PIL.Image import Resampling
 
-# from wand.image import Image as WandImage
-# from wand.display import display as WandDisplay
+
+from wand.api import library
+import wand.color
+import wand.image
 
 import json
 
 DB_FILE = 'intermediates/guardians.sqlite'
 CSV_FILE = 'assets/guardians.csv'
 IMAGE_DIR = 'assets/images'
+
 
 def clean_guardians():
     con = sqlite3.connect(DB_FILE)
@@ -52,6 +55,7 @@ def clean_guardians():
     cur.execute('''DELETE FROM guardians''')
     con.commit()
     con.close()
+
 
 # guardians.csv columns
 # 0 - typeform id
@@ -92,6 +96,7 @@ def resolve_ens(ens):
     ns = ENS.fromWeb3(w3)
     address = ns.address(ens)
     return address
+
 
 def resolve_ens_names():
     address_re = re.compile('(0x[a-fA-F0-9]{40})')
@@ -137,6 +142,7 @@ def download_images():
         con.commit()
     con.close()
 
+
 def substitute_images():
     con = sqlite3.connect(DB_FILE)
     cur_select = con.cursor()
@@ -157,68 +163,31 @@ def substitute_images():
                 con.commit()
 
 
+def svg_to_pil(svgbytes):
 
-# from wand.api import library
-# import wand.color
-# import wand.image
-
-# with wand.image.Image() as image:
-#     with wand.color.Color('transparent') as background_color:
-#         library.MagickSetBackgroundColor(image.wand,
-#                                          background_color.resource)
-#     image.read(blob=svg_file.read(), format="svg")
-#     png_image = image.make_blob("png32")
-#
-
-#import cairosvg
-
-def svg_to_pil(svgbytes, id):
     buf = io.BytesIO(svgbytes)
 
-    #from svglib.svglib import svg2rlg
-    #from reportlab.graphics import renderPM
-    #drawing = svg2rlg(buf)
-    #renderPM.drawToFile(drawing, f"data/guardians/images/{id}.png", fmt="PNG")
+    with wand.image.Image(blob=buf, format="svg", background=wand.color.Color('transparent')) as image:
 
-    #png_image = cairosvg.svg2png(buf)
+        with image.convert('png') as converted:
+            pil = Image.open(io.BytesIO(converted.make_blob()))
+            return pil
 
-    # with open(f"data/guardians/images/{id}.png", "wb") as out:
-    #     out.write(png_image)
-
-
-#     out.write(png_image)
-
-    # with WandImage(blob=buf, format="svg", background=wand.color.Color('transparent')) as image:
-    #
-    #     #image.read(blob=buf, format="svg")
-    #
-    #     with image.convert('png') as converted:
-    #         return Image.open(io.BytesIO(converted.tobytes()), formats=["PNG"])
-
-            #converted.save(filename=f"data/guardians/images/{id}.png")
-
-    #     # png_image = image.make_blob("png")
-    #     # with open(f"data/guardians/images/{id}.png", "wb") as out:
-    #     #     out.write(png_image)
-
-
-
-
-    rlg = svg2rlg(buf)
-    pil = renderPM.drawToPIL(rlg)
-    return pil
 
 def png_to_pil(pngbytes):
     return Image.open(io.BytesIO(pngbytes), formats=["PNG"])
 
+
 def jpeg_to_pil(jpegbytes):
     return Image.open(io.BytesIO(jpegbytes), formats=["JPEG"])
+
 
 def pil_to_png(pil, size):
     out = io.BytesIO()
     pil.thumbnail(size, Resampling.LANCZOS)
     pil.save(out, format="PNG")
     return out.getvalue()
+
 
 def convert_images():
     con = sqlite3.connect(DB_FILE)
@@ -237,7 +206,7 @@ def convert_images():
             pil = None
 
             if extension == 'svg':
-                pil = svg_to_pil(image_src, id)
+                pil = svg_to_pil(image_src)
 
             elif extension == 'png':
                 pil = png_to_pil(image_src)
@@ -254,6 +223,7 @@ def convert_images():
                 con.commit()
 
     con.close()
+
 
 def export_guardians_json():
     con = sqlite3.connect(DB_FILE)
@@ -275,19 +245,21 @@ def export_guardians_json():
     with open('../data/guardians/guardians.json', 'w', encoding='utf-8') as out_file:
         json.dump(guardians, out_file, ensure_ascii=False, indent=4)
 
+
 def export_guardian_images():
     con = sqlite3.connect(DB_FILE)
     cur_select = con.cursor()
     for row in cur_select.execute("SELECT address, image_1x, image_2x, image_3x FROM guardians WHERE image_3x IS NOT NULL ORDER BY address"):
         address, image1, image2, image3 = row
         print(address)
-        # with open(f"../data/guardians/images/{address}_1x.png", 'wb') as f1, open(
-        #         f"../data/guardians/images/{address}_2x.png", 'wb') as f2, open(
-        #     f"../data/guardians/images/{address}_3x.png", 'wb') as f3:
-        #     f1.write(image1)
-        #     f2.write(image2)
-        #     f3.write(image3)
+        with open(f"../data/guardians/images/{address}_1x.png", 'wb') as f1, open(
+                f"../data/guardians/images/{address}_2x.png", 'wb') as f2, open(
+                f"../data/guardians/images/{address}_3x.png", 'wb') as f3:
+            f1.write(image1)
+            f2.write(image2)
+            f3.write(image3)
     con.close()
+
 
 if __name__ == '__main__':
     clean_guardians()
