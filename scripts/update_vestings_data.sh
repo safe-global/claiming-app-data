9df2eaa8ec45a75909cb8e74f393d4baf3ae70a0
Vitaly Katz <vitaly@gnosis.io>
2022-09-26 14:37:50 +0200

    Cleanup

--- scripts/update_vestings_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd vestings
-python exporter.py --clear-db --chain-id ${1} --process-vestings --generate-proofs --output-directory ../data/allocations
-git branch feature/allocations_data_update
-git checkout feature/allocations_data_update
-git add ../data/allocations/1
-git commit -m "Update allocations"
-git push origin feature/allocations_data_update
