75d00d6e3913e2ee04dc520618c453da53bb81f2
Vitaly Katz <vitaly@gnosis.io>
2022-09-26 10:28:42 +0200

    Handle empty dataset correctly

--- vestings/proof_generator.py
+++ vestings/proof_generator.py
@@ -50,5 +50,6 @@ def generate_and_print_root(db: orm.Session, type, chain_id):
     vestings = db.query(VestingModel).filter(VestingModel.type == type and VestingModel.chain_id == chain_id)
     vesting_ids = list(map(lambda vesting: vesting.vesting_id, vestings))
 
-    root = merkle_proof.generate_root(vesting_ids)
-    print(f"{type} root: {root}")
+    if vesting_ids:
+        root = merkle_proof.generate_root(vesting_ids)
+        print(f"{type} root: {root}")
