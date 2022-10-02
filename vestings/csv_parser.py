276d060a3accf470c4f87b994fc0b81535610a92
Vitaly Katz <vitaly@gnosis.io>
2022-10-02 07:42:39 +0200

    Remove code duplication

--- vestings/csv_parser.py
+++ vestings/csv_parser.py
@@ -9,7 +9,7 @@ from web3 import Web3
 
 def parse_vestings_csv(db: orm.Session, type, chain_id, verbose):
 
-    vesting_file = f"assets/{chain_id}/{type}_airdrop.csv"
+    vesting_file = f"assets/{chain_id}/{type}_vestings.csv" if type == "investor" else f"assets/{chain_id}/{type}_airdrop.csv"
 
     with open(vesting_file, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
@@ -33,20 +33,26 @@ def parse_vestings_csv(db: orm.Session, type, chain_id, verbose):
 
             start_date: int
             if "startDate" in row.keys():
-                start_date = int(datetime.datetime.strptime(row["startDate"], "%Y-%m-%dT%H:%M:%S%z").timestamp())
+                if type == "investor":
+                    start_date = int(datetime.datetime.strptime(row["startDate"], "%Y-%m-%dT%H:%M:%S.%fZ").timestamp())
+                else:
+                    start_date = int(datetime.datetime.strptime(row["startDate"], "%Y-%m-%dT%H:%M:%S%z").timestamp())
             else:
                 start_date = int(datetime.datetime.strptime("2018-09-27T10:00:00+00:00", "%Y-%m-%dT%H:%M:%S%z").timestamp())
 
             amount = row["amount"]
+
             curve_type = 0
 
-            vesting = Vesting(None, type, owner, curve_type, duration_weeks, start_date, amount, None)
-
-            USER_AIRDROP_ADDRESS = MAINNET_USER_AIRDROP_ADDRESS if chain_id == 1 else RINKEBY_USER_AIRDROP_ADDRESS
-            ECOSYSTEM_AIRDROP_ADDRESS = MAINNET_ECOSYSTEM_AIRDROP_ADDRESS if chain_id == 1 else RINKEBY_ECOSYSTEM_AIRDROP_ADDRESS
-
-            vesting_id = vesting.calculateHash(USER_AIRDROP_ADDRESS, chain_id) if type == "user" \
-                else vesting.calculateHash(ECOSYSTEM_AIRDROP_ADDRESS, chain_id)
+            vesting_id: str
+            if "vestingId" in row.keys():
+                vesting_id = row["vestingId"]
+            else:
+                vesting = Vesting(None, type, owner, curve_type, duration_weeks, start_date, amount, None)
+                USER_AIRDROP_ADDRESS = MAINNET_USER_AIRDROP_ADDRESS if chain_id == 1 else RINKEBY_USER_AIRDROP_ADDRESS
+                ECOSYSTEM_AIRDROP_ADDRESS = MAINNET_ECOSYSTEM_AIRDROP_ADDRESS if chain_id == 1 else RINKEBY_ECOSYSTEM_AIRDROP_ADDRESS
+                vesting_id = vesting.calculateHash(USER_AIRDROP_ADDRESS, chain_id) if type == "user" \
+                    else vesting.calculateHash(ECOSYSTEM_AIRDROP_ADDRESS, chain_id)
 
             vesting_model = VestingModel(
                 vesting_id=vesting_id,
@@ -97,55 +103,3 @@ def convert_vestings_csv(input_file: str, output_file: str):
                 ofile.write(f"{owner},{duration},{start_date},{amount}")
 
                 line_count += 1
-
-
-def parse_investor_vestings_csv(db: orm.Session, chain_id, verbose):
-
-    vesting_file = f"assets/{chain_id}/investor_airdrop.csv"
-
-    with open(vesting_file, mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        line_count = 0
-
-        print(80 * "-")
-        print(f"Processing investor vestings")
-        print(80 * "-")
-
-        vesting_models = []
-
-        for row in csv_reader:
-
-            vesting_id = row["vestingId"]
-
-            owner = Web3.toChecksumAddress(row["owner"])
-
-            amount = row["amount"]
-
-            start_date = int(datetime.datetime.strptime(row["startDate"], "%Y-%m-%dT%H:%M:%S.%fZ").timestamp())
-
-            duration_weeks = row["duration"]
-
-            curve_type = 0
-
-            vesting_model = VestingModel(
-                vesting_id=vesting_id,
-                chain_id=chain_id,
-                type="investor",
-                owner=owner,
-                curve_type=curve_type,
-                duration_weeks=duration_weeks,
-                start_date=start_date,
-                amount=amount
-            )
-
-            vesting_models.append(vesting_model)
-
-            if verbose:
-                print(f"[{type}] {owner}: {vesting_id}")
-
-            line_count += 1
-
-        db.bulk_save_objects(vesting_models)
-        db.commit()
-
-        print(f'Processed {line_count} investor vestings.')
