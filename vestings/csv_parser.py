d2aac63178d9e2f4b94d14b188bca4f7e3efd4d2
Vitaly Katz <vitaly@gnosis.io>
2022-10-04 10:00:27 +0200

    Cleanup

--- vestings/csv_parser.py
+++ vestings/csv_parser.py
@@ -56,8 +56,7 @@ def parse_vestings_csv(db: orm.Session, type, chain_id, verbose):
             else:
                 user_airdrop_address = MAINNET_USER_AIRDROP_ADDRESS if chain_id == 1 else RINKEBY_USER_AIRDROP_ADDRESS
                 ecosystem_airdrop_address = MAINNET_ECOSYSTEM_AIRDROP_ADDRESS if chain_id == 1 else RINKEBY_ECOSYSTEM_AIRDROP_ADDRESS
-                calculated_vesting_id = vesting.calculateHash(user_airdrop_address, chain_id) if type == "user" \
-                    else vesting.calculateHash(ecosystem_airdrop_address, chain_id)
+                calculated_vesting_id = vesting.calculateHash(user_airdrop_address if type == "user" else ecosystem_airdrop_address, chain_id)
 
             vesting_id: str
 
