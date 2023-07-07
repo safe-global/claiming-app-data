2f26332024d5e2da581bcbcb03a97a0c289365bd
Uxio Fuentefria <uxio0@users.noreply.github.com>
2023-07-07 14:09:50 +0200

    Remove RINKEBY support

--- vestings/addresses.py
+++ vestings/addresses.py
@@ -7,9 +7,6 @@ from constants import (
     MAINNET_INVESTOR_VESTING_POOL_ADDRESS,
     MAINNET_USER_AIRDROP_ADDRESS,
     MAINNET_USER_V2_AIRDROP_ADDRESS,
-    RINKEBY_ECOSYSTEM_AIRDROP_ADDRESS,
-    RINKEBY_USER_AIRDROP_ADDRESS,
-    RINKEBY_USER_V2_AIRDROP_ADDRESS,
 )
 
 
@@ -17,17 +14,14 @@ def get_airdrop_addresses(chain_id):
     return {
         "user": {
             1: MAINNET_USER_AIRDROP_ADDRESS,
-            4: RINKEBY_USER_AIRDROP_ADDRESS,
             5: GOERLI_USER_AIRDROP_ADDRESS,
         }[chain_id],
         "user_v2": {
             1: MAINNET_USER_V2_AIRDROP_ADDRESS,
-            4: RINKEBY_USER_V2_AIRDROP_ADDRESS,
             5: GOERLI_USER_V2_AIRDROP_ADDRESS,
         }[chain_id],
         "ecosystem": {
             1: MAINNET_ECOSYSTEM_AIRDROP_ADDRESS,
-            4: RINKEBY_ECOSYSTEM_AIRDROP_ADDRESS,
             5: GOERLI_ECOSYSTEM_AIRDROP_ADDRESS,
         }[chain_id],
         "investor": {
