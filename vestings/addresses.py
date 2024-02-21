68fa56ee1a2b9661aa417f68c6e3e83bb3ed2c8e
schmanu <manu@safe.global>
2024-02-21 09:35:25 +0100

    feat: add sepolia claiming data

--- vestings/addresses.py
+++ vestings/addresses.py
@@ -9,6 +9,10 @@ from constants import (
     MAINNET_INVESTOR_VESTING_POOL_ADDRESS,
     MAINNET_USER_AIRDROP_ADDRESS,
     MAINNET_USER_V2_AIRDROP_ADDRESS,
+    SEPOLIA_USER_AIRDROP_ADDRESS,
+    SEPOLIA_USER_V2_AIRDROP_ADDRESS,
+    SEPOLIA_ECOSYSTEM_AIRDROP_ADDRESS,
+    SEPOLIA_INVESTOR_VESTING_POOL_ADDRESS,
 )
 from hexbytes import HexBytes
 from vesting import VestingType
@@ -19,17 +23,21 @@ def get_airdrop_addresses(chain_id: int) -> Dict[str, Dict[int, HexBytes]]:
         VestingType.USER: {
             1: MAINNET_USER_AIRDROP_ADDRESS,
             5: GOERLI_USER_AIRDROP_ADDRESS,
+            11155111: SEPOLIA_USER_AIRDROP_ADDRESS,
         }[chain_id],
         VestingType.USER_V2: {
             1: MAINNET_USER_V2_AIRDROP_ADDRESS,
             5: GOERLI_USER_V2_AIRDROP_ADDRESS,
+            11155111: SEPOLIA_USER_V2_AIRDROP_ADDRESS,
         }[chain_id],
         VestingType.ECOSYSTEM: {
             1: MAINNET_ECOSYSTEM_AIRDROP_ADDRESS,
             5: GOERLI_ECOSYSTEM_AIRDROP_ADDRESS,
+            11155111: SEPOLIA_ECOSYSTEM_AIRDROP_ADDRESS,
         }[chain_id],
         VestingType.INVESTOR: {
             1: MAINNET_INVESTOR_VESTING_POOL_ADDRESS,
             5: GOERLI_INVESTOR_VESTING_POOL_ADDRESS,
+            11155111: SEPOLIA_INVESTOR_VESTING_POOL_ADDRESS,
         }[chain_id],
     }
